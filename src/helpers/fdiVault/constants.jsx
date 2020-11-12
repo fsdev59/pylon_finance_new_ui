cebc7c2ffd63c90ad9cc370ac44038cd73ef905f
fsdev59 <fullservicetalentdev@gmail.com>
2020-11-12 12:49:46 -0500

    update the vault address as new

--- src/helpers/fdiVault/constants.jsx
+++ src/helpers/fdiVault/constants.jsx
@@ -1,131 +1,159 @@
-export const ADDR_COMP_VAULT = "0xfB594b9a0cACD9A4e4C506b4d318F7a7AA1caDc9";
-export const ADDR_DAI_VAULT = "0xAF527E70898d8179d38b429f570327E296feAaD3";
-export const ADDR_LEND_VAULT = "0xF910EE6b3924CCF6DebBaB503a7c62B797fB9Fab";
-export const ADDR_LINK_VAULT = "0x884D2D79657BaC17B964bc5D8BdfD80c3712dB74";
-export const ADDR_SNX_VAULT = "0x2dA9a52911bd029306398feaEA4Dd4658247B1f7";
-export const ADDR_TUSD_VAULT = "0x8ffF32c2Fc7E5C904EE05374e0b484d7313ebfD0";
-export const ADDR_UNI_VAULT = "0x35E9866bB7F13EbafeA8CE557faF7D79f6A06DDF";
-export const ADDR_USDC_VAULT = "0x34c107C66D4450b998f0FD28f78BF625b5Db1a4A";
-export const ADDR_USDT_VAULT = "0x11ad73Cd31c3AD6F589cd8AA02Bee3F1f0fE72cC";
-export const ADDR_WBTC_VAULT = "0xCe98E8aE42678595c929d18E0855335635C699B1";
-export const ADDR_WETH_VAULT = "0x36F18Af839A4669edF02308143864c7A6B59A955";
-export const ADDR_YCRV_VAULT = "0xe9Dd93D7C8F237fd57A4236054Bf5d01Fc563e46";
-export const ADDR_YFII_VAULT = "0x0Ce881Ab266e848C2A4D2d873B74Dc7A1138CE6C";
-export const ADDR_YFI_VAULT = "0x769F61BaDB3Bce9d0dC60392759220Ec1DbA79A6";
-export const ADDR_BASED_VAULT = "0xDa4325e70aC8F7D3c14Bb9882E39abE1920f3DC1";
-export const ADDR_BASEDPYLON_VAULT =
-  "0x3Ddbbfb6F2003F670735Bcd44beADd0e0056DF99";
+export const ADDR_COMP_VAULT = "0x694611B0a47a1Ca80a8C575c4201aF5336058a76";
+export const ADDR_DAI_VAULT = "0x3896b3e2dFbED991D0C030440F589F4003ec0327";
+export const ADDR_LEND_VAULT = "0x9a0a8c3aB75544B471aBb6381388CFb96365132C";
+export const ADDR_LINK_VAULT = "0x52d829076dDaeFb5742C26478551F9F1fc4c14FE";
+export const ADDR_SNX_VAULT = "0x7C490872BeF3d0683D8DA030c1AD5e9c13e88b48";
+export const ADDR_TUSD_VAULT = "0xD850DA9816ff484eBaC468D769729449BA99aC69";
+export const ADDR_UNI_VAULT = "0x9a48a188C6DB08C811A8D66A6E474E498FCEb88F";
+export const ADDR_USDC_VAULT = "0x8BAf22bF5788bE6bc099DeFfC0aE6C5206631Dd6";
+export const ADDR_USDT_VAULT = "0x0f14488CBac6CB0f94ef211C80Eb38d470A6A3f9";
+export const ADDR_WBTC_VAULT = "0xdCe902148f12B7D9088Cab2513820742DDD88f5b";
+export const ADDR_WETH_VAULT = "0xb9D01530C7c59437dAA96ABa918e31150589dC00";
+export const ADDR_YCRV_VAULT = "0x65dcA332673A212E45DF020c1BD55a1B9EEe8b70";
+export const ADDR_YFII_VAULT = "0x6EE4d72d3d17A05Dd7F0449E095670119b7D5829";
+export const ADDR_YFI_VAULT = "0xFfc666465c1628B61cD1c119eBcCbF965f6D35a9";
+export const ADDR_BASEDPYLON_VAULT = "0xFA149C8a9230a506FD09b63fe66f1F055e80E89B";
+export const ADDR_AAVE_VAULT = "0xbB2c91C764fC1CF07d5C02671e4EBA4d829A957F";
+export const ADDR_ETH_VAULT = "";
 
 export const ABI_FDI_VAULT =
-  '[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdrawn","type":"event"},{"constant":true,"inputs":[],"name":"_rewardCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"_rewards","outputs":[{"internalType":"uint256","name":"time","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"addressIndices","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"availableWithdraw","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"balance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"deposit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"depositAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"depositBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"governance","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"reward","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"rewardBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_governance","type":"address"}],"name":"setGovernance","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalDeposit","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"withdrawAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]';
+  '[{"inputs":[{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"},{"internalType":"address","name":"_feeAddress","type":"address"},{"internalType":"address","name":"_vaultAddress","type":"address"},{"internalType":"string","name":"name","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"ClaimedReward","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposited","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"SentReward","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdrawn","type":"event"},{"inputs":[],"name":"_rewardCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"_rewards","outputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"startTime","type":"uint256"},{"internalType":"uint256","name":"checkTime","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"_vaultName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"addressIndices","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"availableRewardAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"balance0","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"balance1","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"claimReward","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"claimRewardAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"delayDuration","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"depositAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"depositBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"feeAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"feePermill","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"gov","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"rewardBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardUpdate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"sendReward","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint32","name":"_delayDuration","type":"uint32"}],"name":"setDelayDuration","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_feeAddress","type":"address"}],"name":"setFeeAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint32","name":"_feePermill","type":"uint32"}],"name":"setFeePermill","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_gov","type":"address"}],"name":"setGovernance","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"}],"name":"setToken0","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"}],"name":"setToken1","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_vaultAddress","type":"address"}],"name":"setVaultAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"name","type":"string"}],"name":"setVaultName","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bool","name":"_withdrawable","type":"bool"}],"name":"setWithdrawable","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"token0","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"token1","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDeposit","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"vaultAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawable","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]';
 
 export const FDI_VAULT = [
   {
     title: "Compound",
     tokenName: "COMP",
     iconName: "token_COMP.png",
-    address: "0xfB594b9a0cACD9A4e4C506b4d318F7a7AA1caDc9",
+    address: "0x694611B0a47a1Ca80a8C575c4201aF5336058a76",
+    token0: "0xc00e94Cb662C3520282E6f5717214004A7f26888",
+    token1: "0xc00e94Cb662C3520282E6f5717214004A7f26888",
   },
   {
     title: "DAI",
     tokenName: "DAI",
     iconName: "token_DAI.png",
-    address: "0xAF527E70898d8179d38b429f570327E296feAaD3",
+    address: "0x3896b3e2dFbED991D0C030440F589F4003ec0327",
+    token0: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    token1: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
   },
   {
     title: "ETHLend",
     tokenName: "LEND",
     iconName: "token_LEND.png",
-    address: "0xF910EE6b3924CCF6DebBaB503a7c62B797fB9Fab",
+    address: "0x9a0a8c3aB75544B471aBb6381388CFb96365132C",
+    token0: "0x80fB784B7eD66730e8b1DBd9820aFD29931aab03",
+    token1: "0x80fB784B7eD66730e8b1DBd9820aFD29931aab03",
   },
   {
     title: "Chainlink",
     tokenName: "LINK",
     iconName: "token_LINK.png",
-    address: "0x884D2D79657BaC17B964bc5D8BdfD80c3712dB74",
+    address: "0x52d829076dDaeFb5742C26478551F9F1fc4c14FE",
+    token0: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+    token1: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
   },
   {
     title: "Synthetix Network",
     tokenName: "SNX",
     iconName: "token_SNX.png",
-    address: "0x2dA9a52911bd029306398feaEA4Dd4658247B1f7",
+    address: "0x7C490872BeF3d0683D8DA030c1AD5e9c13e88b48",
+    token0: "0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F",
+    token1: "0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F",
   },
   {
     title: "TrueUSD",
     tokenName: "TUSD",
     iconName: "token_TUSD.png",
-    address: "0x8ffF32c2Fc7E5C904EE05374e0b484d7313ebfD0",
+    address: "0xD850DA9816ff484eBaC468D769729449BA99aC69",
+    token0: "0x0000000000085d4780B73119b644AE5ecd22b376",
+    token1: "0x0000000000085d4780B73119b644AE5ecd22b376",
   },
   {
     title: "Uniswap",
     tokenName: "UNI",
     iconName: "token_UNI.png",
-    address: "0x35E9866bB7F13EbafeA8CE557faF7D79f6A06DDF",
+    address: "0x9a48a188C6DB08C811A8D66A6E474E498FCEb88F",
+    token0: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+    token1: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
   },
   {
     title: "USD Coin",
     tokenName: "USDC",
     iconName: "token_USDC.png",
-    address: "0x34c107C66D4450b998f0FD28f78BF625b5Db1a4A",
+    address: "0x8BAf22bF5788bE6bc099DeFfC0aE6C5206631Dd6",
+    token0: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    token1: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
   },
   {
     title: "Tether USD",
     tokenName: "USDT",
     iconName: "token_USDT.png",
-    address: "0x11ad73Cd31c3AD6F589cd8AA02Bee3F1f0fE72cC",
+    address: "0x0f14488CBac6CB0f94ef211C80Eb38d470A6A3f9",
+    token0: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    token1: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
   },
   {
     title: "Wrapped Bitcoin",
     tokenName: "WBTC",
     iconName: "token_WBTC.png",
-    address: "0xCe98E8aE42678595c929d18E0855335635C699B1",
+    address: "0xdCe902148f12B7D9088Cab2513820742DDD88f5b",
+    token0: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    token1: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
   },
   {
     title: "WETH",
     tokenName: "WETH",
     iconName: "token_WETH.png",
-    address: "0x36F18Af839A4669edF02308143864c7A6B59A955",
+    address: "0xb9D01530C7c59437dAA96ABa918e31150589dC00",
+    token0: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    token1: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
   },
   {
     title: "yCRV",
     tokenName: "YCRV",
     iconName: "token_YCRV.png",
-    address: "0xe9Dd93D7C8F237fd57A4236054Bf5d01Fc563e46",
+    address: "0x65dcA332673A212E45DF020c1BD55a1B9EEe8b70",
+    token0: "0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8",
+    token1: "0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8",
   },
   {
     title: "YeFi.finance",
     tokenName: "YFII",
     iconName: "token_YFII.png",
-    address: "0x0Ce881Ab266e848C2A4D2d873B74Dc7A1138CE6C",
+    address: "0x6EE4d72d3d17A05Dd7F0449E095670119b7D5829",
+    token0: "0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83",
+    token1: "0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83",
   },
   {
     title: "yearn.finance",
     tokenName: "YFI",
     iconName: "token_YFI.png",
-    address: "0x769F61BaDB3Bce9d0dC60392759220Ec1DbA79A6",
+    address: "0xFfc666465c1628B61cD1c119eBcCbF965f6D35a9",
+    token0: "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
+    token1: "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
   },
-  // {
-  //     title: "BASED",
-  //     tokenName: "BASED",
-  //     iconName: "token_BASED.png",
-  //     address: "0xDa4325e70aC8F7D3c14Bb9882E39abE1920f3DC1"
-  // },
   {
     title: "BASED-PYLON",
     tokenName: "BASED-PYLON",
     iconName: "token_BASED.png",
-    address: "0x3Ddbbfb6F2003F670735Bcd44beADd0e0056DF99",
+    address: "0xFA149C8a9230a506FD09b63fe66f1F055e80E89B",
+    token0: "0x68A118Ef45063051Eac49c7e647CE5Ace48a68a5",
+    token1: "0xD7B7d3C0bdA57723Fb54ab95Fd8F9EA033AF37f2",
   },
   {
     title: "Aave",
     tokenName: "AAVE",
     iconName: "token_AAVE.png",
-    address: "",
+    address: "0xbB2c91C764fC1CF07d5C02671e4EBA4d829A957F",
+    token0: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+    token1: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
   },
   {
     title: "Ethereum",
     tokenName: "ETH",
     iconName: "token_ETH.png",
     address: "",
+    token0: "",
+    token1: "",
   }
 ];
