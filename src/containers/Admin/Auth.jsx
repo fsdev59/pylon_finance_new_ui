63c7117f3a854ebe1440f67adaec1127b511d326
fsdev59 <fullservicetalentdev@gmail.com>
2020-11-13 13:28:53 -0500

    add Auth actions

--- src/containers/Admin/Auth.jsx
+++ src/containers/Admin/Auth.jsx
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { toast } from "react-toastify";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faKey } from "@fortawesome/free-solid-svg-icons";
+
+import "./auth.scss";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="container">
-      <div id="login">
-        <form>
-          <fieldset className="clearfix">
-            <p>
-              <span className="fontawesome-user"></span>
-              <input
-                type="text"
-                value="Username"
-                // onBlur="if(this.value == '') this.value = 'Username'"
-                // onFocus="if(this.value == 'Username') this.value = ''"
-                required
-              />
-            </p>
-            <p>
-              <span className="fontawesome-lock"></span>
-              <input
-                type="password"
-                value="Password"
-                // onBlur="if(this.value == '') this.value = 'Password'"
-                // onFocus="if(this.value == 'Password') this.value = ''"
-                required
-              />
-            </p>
-            <p>
-              <input type="submit" value="Sign In" />
-            </p>
-          </fieldset>
-        </form>
-        <p>
-          Not a member? <a href="https://pylon.finance">Sign up now</a>
-          <span className="fontawesome-arrow-right"></span>
-        </p>
+    <div className="auth-container">
+      <div className="auth-wrapper">
+        <div className="auth-row">
+          <FontAwesomeIcon className="icon" icon={faUser} />
+          <input
+            className="input"
+            type="text"
+            placeholder="User"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="auth-row">
+          <FontAwesomeIcon className="icon" icon={faKey} />
+          <input
+            className="input"
+            type="text"
+            placeholder="******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="auth-row">
+          <button className="btn-login">Log In</button>
+        </div>
       </div>
     </div>
   );
--- src/containers/Admin/index.jsx
+++ src/containers/Admin/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import { ConnectedRouter } from "connected-react-router";
+import { useSelector, useDispatch } from "react-redux";
 
 import Auth from "./Auth";
 import Main from "./Main";
 
 const Admin = ({ match }) => {
+  const email = useSelector((state) => state.Auth.email);
+  const auth = useSelector((state) => state.Auth.auth);
+
   const InitialPath = ({ component: Component, authUser, ...rest }) => (
     <Route
       {...rest}
@@ -25,13 +28,13 @@ const Admin = ({ match }) => {
       }
     />
   );
-  console.log(match.url);
+
   return (
     <Switch>
       <InitialPath
         exact
         path={`${match.url}/main`}
-        authUser={{ accessToken: null }}
+        authUser={{ email, auth }}
         component={Main}
       />
       <Route exact path={`${match.url}/login`} component={Auth} />
