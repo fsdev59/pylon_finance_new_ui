e0dd44c282eaf034161927b385bfe446518d5038
fsdev59 <fullservicetalentdev@gmail.com>
2020-11-13 11:53:01 -0500

    fix json

--- src/helpers/pylonVault/constants.jsx
+++ src/helpers/pylonVault/constants.jsx
@@ -13,7 +13,7 @@ export const PYLON_VAULT = [
   {
     title: "PYLON-ETH LP",
     tokenName: "PYLON-ETH",
-    iconName: "token_PYLON.png",
+    iconName: "token_PYLONETH.png",
     address: "0x59d4082ea093060f99b652493fDF0D02f12e518E",
     token0: "0xBe9Ba93515e87C7Bd3A0CEbB9f61AAabE7A77Dd3",
     token1: "0xD7B7d3C0bdA57723Fb54ab95Fd8F9EA033AF37f2",
