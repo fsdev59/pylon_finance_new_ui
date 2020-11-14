b824cf33ce2edbdb62409f09df763c5655cb000d
fsdev59 <fullservicetalentdev@gmail.com>
2020-11-14 11:55:58 -0500

    fix

--- src/redux/page/saga.jsx
+++ src/redux/page/saga.jsx
@@ -584,7 +584,7 @@ export function* getBalance() {
       const balance = yield call(getBalanceAsync, instance, accounts[0])
       const decimal = yield call(getDecimalAsync, instance)
       console.log(balance)
-      const bal = (balance / Math.pow(10, decimal)).toFixed(18)
+      const bal = (balance / Math.pow(10, decimal)).toFixed(decimal)
       console.log(bal)
       callback(bal)
     } else {
@@ -595,7 +595,7 @@ export function* getBalance() {
 
       // const balance = web3.eth.getBalance(accounts[0])
       console.log("ETH Balance", accounts[0], balance)
-      const bal = balance / Math.pow(10, 18)
+      const bal = (balance / Math.pow(10, 18)).toFixed(18)
       console.log("eth balance", bal)
       callback(bal)
     }
@@ -673,11 +673,11 @@ export function* getDepositBalances() {
     if (tokenAddress != "") {
       const tokenInstance = new web3.eth.Contract(TOKEN_ABI, tokenAddress)
       const decimal = yield call(getDecimalAsync, tokenInstance)
-      const depositBalancesVal = depositBalances / Math.pow(10, decimal)
+      const depositBalancesVal = (depositBalances / Math.pow(10, decimal)).toFixed(decimal)
 
       callback(depositBalancesVal)
     } else {
-      const depositBalancesVal = depositBalances / Math.pow(10, 18)
+      const depositBalancesVal = (depositBalances / Math.pow(10, 18)).toFixed(18)
 
       callback(depositBalancesVal)
     }
@@ -698,7 +698,7 @@ export function* getRewardBalances() {
     const rewardBalances = yield call(getRewardBalancesAsync, instance, accounts[0])
 
     // const decimal = yield call(getDecimalAsync, instance)
-    const rewardBalancesVal = rewardBalances / Math.pow(10, 18)
+    const rewardBalancesVal = (rewardBalances / Math.pow(10, 18)).toFixed(18)
 
     callback(rewardBalancesVal)
   })
@@ -720,11 +720,11 @@ export function* getTotalDeposit() {
     if (tokenAddress != "") {
       const tokenInstance = new web3.eth.Contract(TOKEN_ABI, tokenAddress)
       const decimal = yield call(getDecimalAsync, tokenInstance)
-      const totalDepositAmountVal = totalDepositAmount / Math.pow(10, decimal)
+      const totalDepositAmountVal = (totalDepositAmount / Math.pow(10, decimal)).toFixed(decimal)
 
       callback(totalDepositAmountVal)
     } else {
-      const totalDepositAmountVal = totalDepositAmount / Math.pow(10, 18)
+      const totalDepositAmountVal = (totalDepositAmount / Math.pow(10, 18)).toFixed(18)
 
       callback(totalDepositAmountVal)
     }
